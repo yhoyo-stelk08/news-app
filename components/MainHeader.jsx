c79d4cdd68644d682f08385d1dfa60a13ba23435
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-11 14:21:26 +0800

    feat: re-arrange the MainHeader.jsx components.

--- components/MainHeader.jsx
+++ components/MainHeader.jsx
@@ -3,15 +3,17 @@ import Link from 'next/link';
 const MainHeader = () => {
   return (
     <>
-      <header>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/news">News</Link>
-          </li>
-        </ul>
+      <header id="main-header">
+        <div id="logo">
+          <Link href="/">NextNews</Link>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/news">News</Link>
+            </li>
+          </ul>
+        </nav>
       </header>
     </>
   );
