12138738cd8da39618477131f1eca712e3e237ee
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-12 08:27:43 +0800

    feat: create new components NewsList.jsx. create new components NewsList.jsx to render list of news.
     Changes to be committed:
    	new file:   components/NewsList.jsx

---
+++ components/NewsList.jsx
@@ -0,0 +1,15 @@
+const NewsListPage = ({ news }) => {
+  return (
+    <ul className="news-list">
+      {news.map((item) => (
+        <li key={item.id}>
+          <Link href={`/news/${item.slug}`}>
+            <img src={`/images/news/${item.image}`} alt={`${item.title}`} />
+            <span>{item.title}</span>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+export default NewsListPage;
