639a16516cfde31b78bb52daee3e3b6c7bbf0ff3
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-12 09:10:15 +0800

    feat: render latest news in latest news page.
     Changes to be committed:
    	modified:   app/archive/@latest/default.jsx

--- app/archive/@latest/default.jsx
+++ app/archive/@latest/default.jsx
@@ -1,6 +1,13 @@
+import NewsList from '@/components/NewsList';
+import { getLatestNews } from '@/lib/news';
+
 const LatestPage = () => {
+  const latestNews = getLatestNews();
   return (
-    <div>LatestPage</div>
-  )
-}
-export default LatestPage
+    <>
+      <h2 className="text-4xl my-4">Latest News</h2>
+      <NewsList news={latestNews} />
+    </>
+  );
+};
+export default LatestPage;
