20865afefa48c74ba97d43805327d9c925395412
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-12 18:29:24 +0800

    feat: modify the globals.css and Navlinks.jsx.

--- components/NavLinks.jsx
+++ components/NavLinks.jsx
@@ -6,7 +6,7 @@ const NavLinks = ({ href, children }) => {
   const path = usePathname();
   console.log(path)
   return (
-    <Link href={href} className={`${path.startsWith(href) ? ' text-stone-950 bg-[#bcbcb7]' : ''}`}>
+    <Link href={href} className={`${path.startsWith(href) ? ' text-stone-950 bg-[#bcbcb7]' : ' text-yellow-50'}`}>
       {children}
     </Link>
   );
