f5ad91dbf82be0fbae389545fc5956b0d275471d
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-12 09:00:57 +0800

    feat: modified Archive Page. - use function to get year from dummy data and output the year. - use NewsList components in News page. - use NewsList components in archive detail page.
     Changes to be committed:
    	modified:   app/archive/@archive/[year]/page.jsx
    	modified:   app/archive/@archive/page.jsx
    	modified:   app/news/page.jsx

--- app/archive/@archive/page.jsx
+++ app/archive/@archive/page.jsx
@@ -1,6 +1,20 @@
+import { getAvailableNewsYears } from '@/lib/news';
+import Link from 'next/link';
+
 const ArchievePage = () => {
+  const years = getAvailableNewsYears();
   return (
-    <div>ArchievePage</div>
-  )
-}
-export default ArchievePage
+    <header id="archive-header">
+      <nav>
+        <ul>
+          {years.map((year) => (
+            <li key={year}>
+              <Link href={`/archive/${year}`}>{year}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+export default ArchievePage;
--- app/news/page.jsx
+++ app/news/page.jsx
@@ -1,21 +1,11 @@
-import Link from 'next/link';
-
 import { DUMMY_NEWS } from '@/dummy-news';
+import NewsList from '@/components/NewsList';
 
 const NewsPage = () => {
   return (
     <>
       <h1 className={`text-4xl p-0 my-4 font-bold `}>News Page</h1>
-      <ul className="news-list">
-        {DUMMY_NEWS.map((news) => (
-          <li key={news.id}>
-            <Link href={`/news/${news.slug}`}>
-              <img src={`/images/news/${news.image}`} alt={`${news.title}`} />
-              <span>{news.title}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <NewsList news={DUMMY_NEWS} />
     </>
   );
 };
