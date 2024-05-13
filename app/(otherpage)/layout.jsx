cd2d8ef2845368110f11d6778156cf4ad569ea51
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-13 10:37:16 +0800

    feat: create new layout inside (otherpage) routes.
     Changes to be committed:
    	new file:   app/(otherpage)/layout.jsx

---
+++ app/(otherpage)/layout.jsx
@@ -0,0 +1,17 @@
+import '../globals.css';
+import MainHeader from '@/components/MainHeader';
+
+export const metadata = {
+  title: 'Next.js Page Routing & Rendering',
+  description: 'Learn how to route to different pages.',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+          {children}
+      </body>
+    </html>
+  );
+}
