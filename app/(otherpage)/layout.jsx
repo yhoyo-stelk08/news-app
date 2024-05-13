bf4d89412b342ae98d309d077fd9fdfea47184ef
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-13 10:40:12 +0800

    feat: modified the layout of each route groups.

--- app/(otherpage)/layout.jsx
+++ app/(otherpage)/layout.jsx
@@ -10,7 +10,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <div id="page">
+          <MainHeader />
           {children}
+        </div>
       </body>
     </html>
   );
